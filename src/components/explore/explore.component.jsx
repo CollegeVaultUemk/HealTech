876b685f12ec50a04cee1628fcf9253cee4645b7
Parrurox <shaikhparwez963@gmail.com>
2023-04-24 19:17:08 +0530

    cards

--- src/components/explore/explore.component.jsx
+++ src/components/explore/explore.component.jsx
@@ -2,7 +2,31 @@ import React from "react";
 import "./explore.styles.scss";
 
 const Explore = () => {
-  return <div className="explore">Explore</div>;
+  return <div className="explore">Explore
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">something awesome</h2>
+             <p className="card-body"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad ex, consequatur et quos veritatis quas recusandae fugiat deserunt cum inventore saepe odio. Officia corporis numquam quis excepturi deserunt corrupti ullam.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">consult</link> */}
+          <a className="button" href="doctors">consult</a>
+      </div>
+
+
+  
+     
+     </div>
+  
+  
+  
+  
+  </div>;
+   
+   
+
+
+
+
+
 };
 
 export default Explore;
