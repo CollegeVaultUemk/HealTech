b6a42c892910a54ef6eb009fdf237779e626bd1d
Parrurox <shaikhparwez963@gmail.com>
2023-04-24 22:03:38 +0530

    explore section done with doctors routing ��

--- src/components/explore/explore.component.jsx
+++ src/components/explore/explore.component.jsx
@@ -2,25 +2,235 @@ import React from "react";
 import "./explore.styles.scss";
 
 const Explore = () => {
-  return <div className="explore">Explore
+  return <div className="explore">
       <div className="card-content">
          <div className="card">
-          <h2 className="card-title">something awesome</h2>
-             <p className="card-body"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad ex, consequatur et quos veritatis quas recusandae fugiat deserunt cum inventore saepe odio. Officia corporis numquam quis excepturi deserunt corrupti ullam.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">consult</link> */}
-          <a className="button" href="doctors">consult</a>
+          <h2 className="card-title">Allergy and immunology</h2>
+             <p className="card-body"> Allergies and diseases of the immune system.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+        </div>
+       </div> 
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Anesthesiology</h2>
+             <p className="card-body">Pain relief and anesthesia for patients undergoing surgery or other procedures.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+        </div>
+        </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Cardiology</h2>
+             <p className="card-body">Diseases and disorders of the heart and blood vessels.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Dermatology</h2>
+             <p className="card-body"> Disorders of the skin, hair, nails, and mucous membranes.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
       </div>
 
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Emergency Medicine</h2>
+             <p className="card-body">Diagnosis and treatment of urgent and life-threatening conditions.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
 
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Family medicine</h2>
+             <p className="card-body">This specialty provides comprehensive and continuous care for patients of all ages and genders.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Gastroenterology</h2>
+             <p className="card-body">Diseases and disorders of the digestive system, including the stomach, intestines, liver, pancreas, and gallbladder.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Hematology</h2>
+             <p className="card-body">Diseases and disorders of the blood and blood-forming organs.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Infectious Diseases</h2>
+             <p className="card-body">This specialty deals with infections caused by bacteria, viruses, fungi, parasites, or other microorganisms..</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Nephrology</h2>
+             <p className="card-body">This specialty deals with diseases and disorders of the kidneys and urinary system.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Neurology</h2>
+             <p className="card-body">Diseases and disorders of the brain, spinal cord, nerves, and muscles.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Obstetrics and gynecology</h2>
+             <p className="card-body">This specialty deals with the health of women, especially during pregnancy, childbirth, and the reproductive system.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Oncology</h2>
+             <p className="card-body">This specialty deals with the diagnosis and treatment of cancer.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Ophthalmology</h2>
+             <p className="card-body">This specialty deals with diseases and disorders of the eye and vision</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Orthopedic surgery</h2>
+             <p className="card-body">Deals with diseases and disorders of the bones, joints, muscles, tendons, and ligaments.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Otolaryngology</h2>
+             <p className="card-body">This specialty deals with diseases and disorders of the ear, nose, throat, head, and neck.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Pediatrics</h2>
+             <p className="card-body">This specialty deals with the health and development of children from birth to adolescence.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Psychiatry</h2>
+             <p className="card-body">Diagnosis and treatment of mental, emotional, and behavioral disorders.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Pulmonology</h2>
+             <p className="card-body">This specialty deals with diseases and disorders of the lungs and respiratory system..</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Radiology</h2>
+             <p className="card-body">This specialty uses imaging techniques such as X-rays, ultrasound, MRI, CT scan, etc. to diagnose and treat diseases.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Rheumatology</h2>
+             <p className="card-body">This specialty deals with diseases and disorders of the joints, muscles, bones, and connective tissues.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Surgery</h2>
+             <p className="card-body">This is a broad category that includes various subspecialties that perform surgical procedures on different parts of the body or organs. Some examples are general surgery, vascular surgery, plastic surgery, neurosurgery, cardiac surgery, etc.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Urology</h2>
+             <p className="card-body">This specialty deals with diseases and disorders of the male and female urinary tract and t</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Cardiology</h2>
+             <p className="card-body">Diseases and disorders of the heart and blood vessels.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+      <div className="card-content">
+         <div className="card">
+          <h2 className="card-title">Cardiology</h2>
+             <p className="card-body">Diseases and disorders of the heart and blood vessels.</p>
+           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
+          <a className="button" href="doctors">Consult</a>
+      </div>
+      </div>
+
+</div>;
   
-     
-     </div>
-  
-  
-  
-  
-  </div>;
-   
+    
+
    
 
 
