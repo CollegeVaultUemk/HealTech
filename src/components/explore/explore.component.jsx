ae457a56e9db7e01c111cae6912200ac6d567efb
Parrurox <shaikhparwez963@gmail.com>
2023-04-26 11:19:08 +0530

    updated explore section

--- src/components/explore/explore.component.jsx
+++ src/components/explore/explore.component.jsx
@@ -7,8 +7,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Allergy and immunology</h2>
              <p className="card-body"> Allergies and diseases of the immune system.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.castleconnolly.com/topics/allergy-immunology/what-is-allergy-immunology" target="_blank">Learn more..</a>
         </div>
        </div> 
 
@@ -16,8 +16,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Anesthesiology</h2>
              <p className="card-body">Pain relief and anesthesia for patients undergoing surgery or other procedures.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.news-medical.net/health/What-is-Anesthesiology.aspx" target="_blank">Learn more..</a>
         </div>
         </div>
 
@@ -25,16 +25,16 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Cardiology</h2>
              <p className="card-body">Diseases and disorders of the heart and blood vessels.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.healio.com/news/cardiology/20120629/what-is-cardiology" target="_blank">Learn more..</a>
       </div>
       </div>
       <div className="card-content">
          <div className="card">
           <h2 className="card-title">Dermatology</h2>
              <p className="card-body"> Disorders of the skin, hair, nails, and mucous membranes.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.news-medical.net/health/What-is-Dermatology.aspx" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -42,17 +42,17 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Emergency Medicine</h2>
              <p className="card-body">Diagnosis and treatment of urgent and life-threatening conditions.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://en.wikipedia.org/wiki/Emergency_medicine" target="_blank">Learn more..</a>
       </div>
       </div>
 
       <div className="card-content">
          <div className="card">
-          <h2 className="card-title">Family medicine</h2>
+          <h2 className="card-title">Family Medicine</h2>
              <p className="card-body">This specialty provides comprehensive and continuous care for patients of all ages and genders.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.aafp.org/about/dive-into-family-medicine/family-medicine-speciality.html#:~:text=Family%20Medicine%3A%20The%20Center%20of,ages%20%E2%80%94%20from%20newborns%20to%20seniors." target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -60,8 +60,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Gastroenterology</h2>
              <p className="card-body">Diseases and disorders of the digestive system, including the stomach, intestines, liver, pancreas, and gallbladder.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://en.wikipedia.org/wiki/Gastroenterology" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -69,8 +69,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Hematology</h2>
              <p className="card-body">Diseases and disorders of the blood and blood-forming organs.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.hopkinsmedicine.org/health/treatment-tests-and-therapies/hematology#:~:text=What%20is%20hematology%3F,blood%20and%20bone%20marrow%20cells." target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -78,8 +78,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Infectious Diseases</h2>
              <p className="card-body">This specialty deals with infections caused by bacteria, viruses, fungi, parasites, or other microorganisms..</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://health.ri.gov/diseases/infectious/" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -87,8 +87,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Nephrology</h2>
              <p className="card-body">This specialty deals with diseases and disorders of the kidneys and urinary system.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.narayanahealth.org/specialities/nephrology" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -96,8 +96,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Neurology</h2>
              <p className="card-body">Diseases and disorders of the brain, spinal cord, nerves, and muscles.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.britannica.com/science/neurology" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -105,8 +105,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Obstetrics and gynecology</h2>
              <p className="card-body">This specialty deals with the health of women, especially during pregnancy, childbirth, and the reproductive system.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.britannica.com/" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -114,8 +114,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Oncology</h2>
              <p className="card-body">This specialty deals with the diagnosis and treatment of cancer.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.britannica.com/" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -123,8 +123,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Ophthalmology</h2>
              <p className="card-body">This specialty deals with diseases and disorders of the eye and vision</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.britannica.com/" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -132,8 +132,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Orthopedic surgery</h2>
              <p className="card-body">Deals with diseases and disorders of the bones, joints, muscles, tendons, and ligaments.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.britannica.com/" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -141,8 +141,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Otolaryngology</h2>
              <p className="card-body">This specialty deals with diseases and disorders of the ear, nose, throat, head, and neck.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.britannica.com/" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -150,8 +150,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Pediatrics</h2>
              <p className="card-body">This specialty deals with the health and development of children from birth to adolescence.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.britannica.com/" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -159,8 +159,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Psychiatry</h2>
              <p className="card-body">Diagnosis and treatment of mental, emotional, and behavioral disorders.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.britannica.com/" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -168,8 +168,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Pulmonology</h2>
              <p className="card-body">This specialty deals with diseases and disorders of the lungs and respiratory system..</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.britannica.com/" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -177,8 +177,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Radiology</h2>
              <p className="card-body">This specialty uses imaging techniques such as X-rays, ultrasound, MRI, CT scan, etc. to diagnose and treat diseases.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.britannica.com/" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -186,8 +186,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Rheumatology</h2>
              <p className="card-body">This specialty deals with diseases and disorders of the joints, muscles, bones, and connective tissues.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.britannica.com/" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -195,8 +195,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Surgery</h2>
              <p className="card-body">This is a broad category that includes various subspecialties that perform surgical procedures on different parts of the body or organs. Some examples are general surgery, vascular surgery, plastic surgery, neurosurgery, cardiac surgery, etc.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.britannica.com/" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -204,8 +204,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Urology</h2>
              <p className="card-body">This specialty deals with diseases and disorders of the male and female urinary tract and t</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.britannica.com/" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -213,8 +213,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Cardiology</h2>
              <p className="card-body">Diseases and disorders of the heart and blood vessels.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.britannica.com/" target="_blank">Learn more..</a>
       </div>
       </div>
 
@@ -222,8 +222,8 @@ const Explore = () => {
          <div className="card">
           <h2 className="card-title">Cardiology</h2>
              <p className="card-body">Diseases and disorders of the heart and blood vessels.</p>
-           {/* <link className="card-link" to="/dashboard/doctors">Consult</link> */}
-          <a className="button" href="doctors">Consult</a>
+           {/* <link className="card-link" to="/dashboard/https://www.britannica.com/">Learn more..</link> */}
+          <a className="button" href="https://www.britannica.com/" target="_blank">Learn more..</a>
       </div>
       </div>
 
